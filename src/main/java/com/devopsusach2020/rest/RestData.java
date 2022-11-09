844df276979bbf9b656aa2e2918cb3389bb40669
Marcelo Morales V <marcelo.morales@ixcomercio.com>
2022-11-09 00:05:35 -0300

    Correcciones OK

--- src/main/java/com/devopsusach2020/rest/RestData.java
+++ src/main/java/com/devopsusach2020/rest/RestData.java
@@ -20,9 +20,9 @@ import com.google.gson.Gson;
 @RequestMapping(path = "/rest/mscovid")
 public class RestData {
 	
-	private final static Logger LOGGER = Logger.getLogger("devops.subnivel.Control");
+	private static final Logger LOGGER = Logger.getLogger("devops.subnivel.Control");
 
-	
+
 	@GetMapping(path = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody Pais getData(@RequestParam(name = "msg") String message){
 		
@@ -46,21 +46,25 @@ public class RestData {
 		int death = 0;
 		int recovered = 0;
 		Gson gson = new Gson();
-        Pais[] estados = gson.fromJson(call.getBody().toLowerCase(), Pais[].class);
+		String body = call.getBody();
+		if(body != null){
+			Pais[] estados = gson.fromJson(body.toLowerCase(), Pais[].class);
 
-        for(Pais estado : estados) {
-        	response.setDate(estado.getDate());
-        	response.setActive(estado.getActive());
-        	confirmed += estado.getConfirmed();
-        	death += estado.getDeaths();
-        	recovered += estado.getRecovered();
-        }
+			for(Pais estado : estados) {
+				response.setDate(estado.getDate());
+				response.setActive(estado.getActive());
+				confirmed += estado.getConfirmed();
+				death += estado.getDeaths();
+				recovered += estado.getRecovered();
+			}
+			
+			response.setConfirmed(confirmed);
+			response.setDeaths(death);
+			response.setRecovered(recovered);
+			response.setCountry(message);
+			response.setMensaje("ok");
+		}
         
-    	response.setConfirmed(confirmed);
-    	response.setDeaths(death);
-    	response.setRecovered(recovered);
-    	response.setCountry(message);
-    	response.setMensaje("ok");
 
 		return response;		
 	}
@@ -75,10 +79,13 @@ public class RestData {
 	    ResponseEntity<String> call= restTemplate.getForEntity("https://api.covid19api.com/world/total" ,String.class);
 	    Mundial response = new Mundial();
 		Gson gson = new Gson();
-        Mundial estado = gson.fromJson(call.getBody().toLowerCase(), Mundial.class);
-        response.setTotalConfirmed(estado.getTotalConfirmed());
-        response.setTotalDeaths(estado.getTotalDeaths());
-        response.setTotalRecovered(estado.getTotalRecovered());
+		String body = call.getBody();
+		if(body != null){
+			Mundial estado = gson.fromJson(body.toLowerCase(), Mundial.class);
+			response.setTotalConfirmed(estado.getTotalConfirmed());
+			response.setTotalDeaths(estado.getTotalDeaths());
+			response.setTotalRecovered(estado.getTotalRecovered());
+		}
 
 		return response;		
 	}
--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -1,13 +1,17 @@
 package com.devopsusach2020;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.devopsusach2020.rest.RestData;
 
 @SpringBootTest
 class DevOpsUsach2020ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		RestData data = new RestData();
+		Assertions.assertNotNull(data.getData("Chile"));
 	}
 
 }
